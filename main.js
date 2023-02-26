912deaecacb9e3551a9d203683e8561d602fd5c8
ErikaGalanCont <102776229+ErikaGalanCont@users.noreply.github.com>
2023-02-26 10:31:32 -0500

    pares e impares

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
-//Realizar un programa que permita escribir los 100 primeros pares.
+//Realizar un programa que lea N números, calcule y escriba la suma de los pares y el producto de los impares
+
+let veces = Number(prompt("cuantos numeros vas a ingresar?"));
 
 
-for (let i=0; i<200; i+=2) {
-    
-    console.log(i);
-    
+let paresDos = 0
+let imparesDos = 1
+
+for (let i=0; i<veces; i++) { 
+    let numero = Number(prompt("ingresa el numero"));
+    if (numero%2 ==0) {
+        paresDos += numero
+    }else{
+        imparesDos *= numero 
+    }
 }
+
+console.log(paresDos);
+console.log(imparesDos);
+
+
+
+
+
+
+
+
+
+
