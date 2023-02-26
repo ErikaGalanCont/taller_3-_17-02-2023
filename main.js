d9ad181b0a028739d28694412425b4a053670e2f
ErikaGalanCont <102776229+ErikaGalanCont@users.noreply.github.com>
2023-02-26 08:59:41 -0500

    maximo numero teclado

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
+//	Realice un programa que calcule el máximo de N números leídos desde teclado.
 
-let num1 = Number(prompt("Ingresa el primer numero"));
-let num2 = Number(prompt("Ingresa el segundo numero"));
+let veces = Number(prompt("cuantos numeros vas a ingresar?"));
 
-if(num1>num2){
-    console.log("El numero " + num1 + " es mayor que el " + num2 );
-}else if(num2<num1){
-    console.log("El numero " + num2 + " es mayor que el " + num1 );
-}else{
-    console.log("Los numeros son iguales");
+
+let numeros = []
+for (let i=0; i<veces; i++) { 
+    numeros.push (Number(prompt("ingresa el numero")));
 }
+
+let mayor = Math.max(...numeros);
+console.log(numeros);
+
+// los puntos son para soltar los numeros del arreglo y asi poder sacar el mayor de los numeros
+
+alert ("el mayor de los numeros es " + mayor );
