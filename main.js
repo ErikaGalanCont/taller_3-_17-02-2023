735f9d917180e9f155ce048d2afba2ab11c8841d
ErikaGalanCont <102776229+ErikaGalanCont@users.noreply.github.com>
2023-02-26 09:04:55 -0500

    suma dos numeros

--- main.js
+++ main.js
@@ -1,16 +1,7 @@
-//	Realice un programa que calcule el máximo de N números leídos desde teclado.
+//Realizar un programa que pueda sumar dos números leídos por teclado y escribir el resultado.
 
-let veces = Number(prompt("cuantos numeros vas a ingresar?"));
+let num1 = Number(prompt("Ingresa el primer numero"));
+let num2 = Number(prompt("Ingresa el primer numero"));
 
-
-let numeros = []
-for (let i=0; i<veces; i++) { 
-    numeros.push (Number(prompt("ingresa el numero")));
-}
-
-let mayor = Math.max(...numeros);
-console.log(numeros);
-
-// los puntos son para soltar los numeros del arreglo y asi poder sacar el mayor de los numeros
-
-alert ("el mayor de los numeros es " + mayor );
+let suma = num1 + num2
+alert("el resultado es " + suma);
