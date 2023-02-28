1d4b3ddcc943118d760d6d6355ab81be5c14ae5a
ErikaGalanCont <102776229+ErikaGalanCont@users.noreply.github.com>
2023-02-28 07:23:10 -0500

    ultimo

--- main.js
+++ main.js
@@ -1,24 +1,19 @@
-//Realizar un programa que lea N números, calcule y escriba la suma de los pares y el producto de los impares
+//Dados dos números enteros positivos N y D, se dice que D es un divisor de N si el resto de dividir N entre D es 0. Se dice que un número N es perfecto si la suma de sus divisores (excluido el propio N) es N. Por ejemplo 28 es perfecto, pues sus divisores (excluido elv28) son: 1, 2, 4, 7 y 14 y su suma es 1+2+4+7+14=28. Hacer un organigrama que dado un número N nos diga si es o no perfecto.
 
-let veces = Number(prompt("cuantos numeros vas a ingresar?"));
+let n = Number(prompt("ingresa un numero entero"));
+let sumaDivisores = 0;
 
-
-let paresDos = 0
-let imparesDos = 1
-
-for (let i=0; i<veces; i++) { 
-    let numero = Number(prompt("ingresa el numero"));
-    if (numero%2 ==0) {
-        paresDos += numero
-    }else{
-        imparesDos *= numero 
+for (let i = 1; i < n; i++) {
+    if (n % i === 0) {
+    sumaDivisores += i;
     }
 }
 
-console.log(paresDos);
-console.log(imparesDos);
-
-
+if (sumaDivisores === n) {
+    console.log(`${n} es un número perfecto`);
+} else {
+    console.log(`${n} no es un número perfecto`);
+}
 
 
 
